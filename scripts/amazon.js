e0a22b99607c43471d2fea9720aa9a43e806b09f
SuperSimpleDev <kaushalbdrkunwar@gmail.com>
2024-10-07 14:06:22 +0545

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -85,7 +85,14 @@ products.forEach((product) => {
 
             }
 
-            console.log(cart);
+            let cartQuantity = 0;
+
+            cart.forEach((item) =>{
+                    cartQuantity += item.quantity;
+            });
+
+            document.querySelector('.js-cart-quantity')
+            .innerHTML = cartQuantity;
 
         });
     });

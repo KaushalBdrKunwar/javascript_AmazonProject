a712b266be84ff107df96270b144a0747a935f10
SuperSimpleDev <kaushalbdrkunwar@gmail.com>
2024-10-17 10:21:27 +0545

    Generate HTML for the order summary

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,7 +1,7 @@
 
 // Steps of doing Javascripts
 //......#1....Save the data i.e. product information in our case.
-import {cart} from '../data/cart.js';//importing module at the top
+import {cart, addToCart} from '../data/cart.js';//importing module at the top
 import { products } from '../data/products.js';//avoids naming conflicts.
 
 let productsHTML = '';
@@ -61,40 +61,27 @@ products.forEach((product) => {
                     `; 
 } );   
 
+
  document.querySelector('.js-products-grid').
  innerHTML = productsHTML;
+
+function updateCartQuantity(){
+    let cartQuantity = 0;
+
+    cart.forEach((cartItem) =>{
+            cartQuantity += cartItem.quantity;
+    });
+
+    document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+}
 
  document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click',() =>{
            const productId =  button.dataset.productId;
-
-           let matchingItem;
-
-            cart.forEach((item) => {
-                if(productId === item.productId){
-                    matchingItem = item;
-                }
-            });
-
-            if(matchingItem){
-                matchingItem.quantity += 1;
-            } else{
-                cart.push({
-                    productId : productId,
-                    quantity:1
-                   });
-
-            }
-
-            let cartQuantity = 0;
-
-            cart.forEach((item) =>{
-                    cartQuantity += item.quantity;
-            });
-
-            document.querySelector('.js-cart-quantity')
-            .innerHTML = cartQuantity;
+            addToCart(productId);
+            updateCartQuantity();
 
         });
     });
